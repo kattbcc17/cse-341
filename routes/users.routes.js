20dd92d04198e4445868f614ff8ace9268f8a442
Kattia <kattbcc17@byui.edu>
2024-10-23 22:33:32 -0300

    feat: add data validation for contact routes using express-validator

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,18 +1,38 @@
 import express from 'express'
 import { getAll, getSingle, createContact, updateContact, deleteContact } from '../controllers/contacts.controller.js'
-
+import { body, param } from 'express-validator';
 
 const router = express.Router()
 
-router.get('/api/users/all', getAll)
+// Validation helper for ID
+const validateId = [
+    param('id').isMongoId().withMessage('Invalid ID format')
+];
 
-router.get('/api/users/:id', getSingle);
+// Validation for creating and updating contacts
+const validateContact = [
+    body('firstName').notEmpty().withMessage('First name is required'),
+    body('lastName').notEmpty().withMessage('Last name is required'),
+    body('email').isEmail().withMessage('Email is invalid'),
+    body('favoriteColor').notEmpty().withMessage('Favorite color is required'),
+    body('birthday').isDate().withMessage('Birthday must be a valid date')
+];
 
-router.post("/contacts", createContact);
 
-router.put("/contacts/:id", updateContact);
+// Get all contacts
+router.get('/api/users/all', getAll);
 
-router.delete("/contacts/:id", deleteContact);
+// Get a single contact by ID
+router.get('/api/users/:id', validateId, getSingle);
+
+// Create a new contact
+router.post("/contacts", validateContact, createContact);
+
+// Update an existing contact
+router.put("/contacts/:id", [...validateId, ...validateContact], updateContact);
+
+// Delete a contact
+router.delete("/contacts/:id", validateId, deleteContact);
 
 
 export default router;
